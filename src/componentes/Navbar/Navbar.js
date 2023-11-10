7e578fbf56c0ed166fa736fa5a87818f55d0de55
Lu <luciaverge12@hotmail.com>
2023-11-10 20:57:27 -0300

    cambio colores, favicon y agrego navbar

---
+++ src/componentes/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+      <div class="nav">
+        <div class="input-nav">
+        <a href="#">Alarmas</a>
+        </div>
+        <div class="input-nav">
+        <a href="#">Filtrar</a>
+        </div>
+        <div class="input-nav">
+        <a href="#">Configuracion</a>
+        </div>
+        <div class="input-nav">
+        <input type="text" />
+        </div>
+      </div>
+    );
+}
+
+export default Navbar;
